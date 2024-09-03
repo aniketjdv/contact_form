35446c797c65890c335fc934b28c4d6d18f423e4
Aniket <aniketjadhav310@gmail.com>
2024-09-03 19:34:50 +0530

    forms working done and database link done

--- contact/contactform/views.py
+++ contact/contactform/views.py
@@ -1,6 +1,15 @@
-from django.shortcuts import render
-
+from django.shortcuts import redirect, render
+from .forms import ContactForm
+from api.models import Contact_Enquriy
 # Create your views here.
 
 def contact_form(request):
-    return render(request,'contact.html')
+    enq=None
+    if request.method == 'POST':
+        form=ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('success') 
+    else:
+        form=ContactForm()
+    return render(request,'contact.html',{'form':form})
